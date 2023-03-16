58857f10aea94ba0656fb2ac38e1fb7b19c30d38
alekseylantsov <lalex239@gmail.com>
2023-03-16 19:12:46 +0400

    More comments for the new files

--- controller/cli.py
+++ controller/cli.py
@@ -9,8 +9,10 @@ def game_cli_action(player: Player, board: [tuple[Card], list[Card]], bet):
     print(f"Your stack is {player.stack}. Your current bet is {player.get_bet}")
     print(f"Your variants: {player.ask_for_a_decision(bet)}. The bet is {bet}")
     while True:
+        # Make sure the player entered exactly 1 or two words
         if len(input_values := input().split(' ')) not in [1, 2]:
             continue
+        # Try to post a bet if we have two values, making sure the move is allowed, and the bet is valid
         elif len(input_values) == 2:
             decision, bet = input_values
             try:
@@ -22,6 +24,7 @@ def game_cli_action(player: Player, board: [tuple[Card], list[Card]], bet):
                 print("You must enter positive bet")
             except TooSmallBetError:
                 print("The bet must not be lower than the current bet")
+        # Try to perform a single-word action, making sure the move is allowed
         else:
             try:
                 player.decide(input_values[0])
@@ -31,7 +34,10 @@ def game_cli_action(player: Player, board: [tuple[Card], list[Card]], bet):
 
 
 def ask_for_int_input(parameter: str, domain: tuple[int, int], suggested_range: tuple[int, int]) -> int:
-    """Game configuration"""
+    """
+    Ask for int value for game configuration having suggested and valid ranges
+    It is checked that the value can be converted to int, and that it is within the domain.
+    """
     prompt = f"Please, input the {parameter} from {domain[0]} to {domain[1]}.\r\n" \
              f"We suggest the range between {suggested_range[0]} and {suggested_range[1]}.\r\n"
     while True:
@@ -46,14 +52,21 @@ def ask_for_int_input(parameter: str, domain: tuple[int, int], suggested_range: 
             continue
 
 
-def ask_for_name(prompt='') -> str:
-    prompt = prompt or "Hi! What's your name?\r\n"
+def ask_for_str_input(prompt="Hi! What's your name?\r\n") -> str:
+    """
+    Ask for a string input.
+    Used mainly for the name input, so the default prompt is about the name
+    No validations except that the string is not empty
+    """
     while not (name := input(prompt)):
         continue
     return name
 
 
 def ask_for_bool_input(prompt) -> bool:
+    """
+    Asks for a bool value. Ensures that user enters 'y' or 'n'. The case doesn't matter.
+    """
     print(prompt)
     bool_prompt = "Please, enter 'y' or 'n'!\n"
     # prompt = " ".join([prompt, bool_prompt])
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pprint import pprint
 from secrets import SystemRandom
 
 from config import DEFAULT_PLAYERS_NUM, DEFAULT_BUY_IN, DEFAULT_BLIND
-from controller.cli import ask_for_int_input, ask_for_name, ask_for_bool_input
+from controller.cli import ask_for_int_input, ask_for_str_input, ask_for_bool_input
 from data.constants import NAMES
 from entities.combinations import best_hand
 from entities.game import Game, Player
@@ -12,15 +12,20 @@ random = SystemRandom()
 
 
 def start_a_game(number_of_players, init_chips, blind, player_name):
+    # create AI players and assign uniq names to them
     names = NAMES.copy()
     players = [Player(init_chips, name=names.pop(random.randint(0, len(names)))) for _ in range(number_of_players - 1)]
+    # add one human player
     players.append(Player(init_chips, is_ai=False, name=player_name))
+    # create the game and add the players
     game = Game(blind, init_chips, continuous=False)
     for player in players:
         game.add_player(player)
+    # start a new game round while there are at least 2 players, including AI
     while True:
         try:
             new_round = game.new_round()
+            # print winners and their hands if there are more than ont player left at the end of the round
             if len(new_round.active_players) > 1:
                 for player in players:
                     if player in new_round.players:
@@ -28,21 +33,25 @@ def start_a_game(number_of_players, init_chips, blind, player_name):
                         print(best_hand(new_round.board + player.hand) if player.is_active else None)
                         if player in new_round.winners:
                             print('Player is winner')
+            # print the status of the round at its end
             pprint(new_round.get_status(), indent=2)
             print()
         except errors.NotEnoughPlayers:
             break
+    # list the players and their stacks at the end of the game
     for player in players:
-        print(player.stack)
+        print(f"{player.name}'s stack is {player.stack}")
 
 
 def main():
-    player_name = ask_for_name()
+    player_name = ask_for_str_input()
     quick_game = ask_for_bool_input('Would you like to start a quick game?')
+    # create a single-player game with default params
     if quick_game:
         number_of_players = DEFAULT_PLAYERS_NUM
         init_chips = DEFAULT_BUY_IN
         blind_size = DEFAULT_BLIND
+    # create a custom single-player game
     else:
         number_of_players = ask_for_int_input('number of players', (2, 7), (3, 5))
         init_chips = ask_for_int_input('initial chips', (100, 100_000), (300, 10_000))
